00ff770b60831ce143e07d884163a121bacdd8f2
Juan Luis Ochoa <jlprofesor8@gmail.com>
2022-05-16 22:05:31 +0200

    Primer commit

---
+++ WebApiBiblioteca/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiBiblioteca.Entidades;
+using WebApiBiblioteca.Entidades.Seed;
+
+namespace WebApiBiblioteca
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SeedData.Seed(modelBuilder);
+        }
+
+        public DbSet<Editorial> Editoriales { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+    }
+}
---
+++ WebApiBiblioteca/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiBiblioteca;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Registramos en el sistema de inyección de dependencias de la aplicación el ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+            opciones.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
