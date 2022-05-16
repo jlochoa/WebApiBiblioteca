6acb48af11df83e440a67a6ebeb3aa404377e8c5
Juan Luis Ochoa <jlprofesor8@gmail.com>
2022-05-16 22:15:03 +0200

    Comentarios agregados

--- WebApiBiblioteca/Entidades/Editorial.cs
+++ WebApiBiblioteca/Entidades/Editorial.cs
@@ -5,6 +5,7 @@ namespace WebApiBiblioteca.Entidades
     public class Editorial
     {
         public int EditorialId { get; set; }
+        // Obligatorio y ancho máximo
         [Required]
         [StringLength(50)]
         public string Nombre { get; set; }
